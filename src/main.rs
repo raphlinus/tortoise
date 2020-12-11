e0b2adf92589af1b477201c3d052295bee6040ec
Raph Levien <raph.levien@gmail.com>
2020-12-10 21:27:16 -0800

    Track names
    Just keep a hashmap of names.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use rspirv::binary::{Consumer, ParseAction, Parser};
-use rspirv::dr::{Instruction, ModuleHeader};
 
-struct MyConsumer;
+use std::collections::HashMap;
+
+use rspirv::binary::{Consumer, ParseAction, Parser};
+use rspirv::dr::{Instruction, ModuleHeader, Operand};
+use rspirv::spirv::{Op, Word};
+
+#[derive(Default)]
+struct MyConsumer {
+    names: HashMap<Word, String>,
+}
 
 impl Consumer for MyConsumer {
     fn initialize(&mut self) -> ParseAction {
@@ -19,6 +26,15 @@ impl Consumer for MyConsumer {
 
     fn consume_instruction(&mut self, inst: Instruction) -> ParseAction {
         println!("inst: {:?}", inst);
+        match inst.class.opcode {
+            Op::Name => {
+                println!("operands: {:?}", inst.operands);
+                if let (Operand::IdRef(id), Operand::LiteralString(s)) = (&inst.operands[0], &inst.operands[1]) {
+                    self.names.insert(*id, s.clone());
+                }
+            }
+            _ => (),
+        }
         ParseAction::Continue
     }
 }
@@ -26,7 +42,8 @@ impl Consumer for MyConsumer {
 fn main() {
     let filename = std::env::args().skip(1).next().expect("need spv file name");
     let bytes = std::fs::read(filename).unwrap();
-    let mut consumer = MyConsumer;
+    let mut consumer = MyConsumer::default();
     let parser = Parser::new(&bytes, &mut consumer);
     parser.parse().unwrap();
+    println!("{:?}", consumer.names);
 }
